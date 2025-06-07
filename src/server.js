b68ee1b8b5017a52dec4bdbe06b34eecf27e595e
Gabriel Soares <gabrielsoacc@gmail.com>
2025-06-07 10:13:21 -0300

    fix: added validation method http type for end-point test

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 http.createServer(
     (request, response) => {
-        if(request.url === '/hello-world'){
+        if(request.url === '/hello-world' && request.method === 'GET'){
             response.writeHead(200, {"Content-Type": 'Application/Json'});
             response.end(JSON.stringify({'message':'Hello World'}));
         }
